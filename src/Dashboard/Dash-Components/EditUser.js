2b7bd0a3e8be8727e159792f6a70ca8ed9550006
emmasam <findm4@gmail.com>
2022-06-11 13:44:44 +0100

    driver

--- src/Dashboard/Dash-Components/EditUser.js
+++ src/Dashboard/Dash-Components/EditUser.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 
 function EditUser(props) {
-  const [fName, setFName] = useState("");
+  const [fName, setFName] = useState(props.data.firstname);
+  const [lName, setLName] = useState(props.data.lName);
+  const [email, setEmail] = useState(props.data.email);
+  const [phone, setPhone] = useState(props.data.phone);
+  const [city, setCity] = useState(props.data.city);
 
   const UpdateUser = (e) => {};
+
   return (
     <div className="overlay position-fixed d-flex justify-content-center">
       <div className="user-modal align-self-center p-3 d-flex flex-column">
@@ -15,18 +20,38 @@ function EditUser(props) {
           <input
             type="text"
             className="input-home"
-            value={props.data.firstname}
+            value={fName}
             onChange={(e) => setFName(e.target.value)}
             name="fName"
           />
           <input
             type="text"
             className="input-home"
-            value={props.data.lastname}
+            value={lName}
+            name="lName"
+            onChange={(e) => setLName(e.target.value)}
           />
-          <input type="text" className="input-home" value={props.data.email} />
-          <input type="text" className="input-home" value={props.data.phone} />
-          <input type="text" className="input-home" value={props.data.city} />
+          <input
+            type="text"
+            className="input-home"
+            value={email}
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            className="input-home"
+            value={phone}
+            name="phone"
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <input
+            type="text"
+            className="input-home"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            name="city"
+          />
 
           <button className="my_btn w900">Save</button>
         </form>
