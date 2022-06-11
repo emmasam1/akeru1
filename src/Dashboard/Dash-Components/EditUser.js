a9094e18a5285dc3168086e7ac994cec21694f50
emmasam <findm4@gmail.com>
2022-06-11 13:21:29 +0100

    edit user

--- src/Dashboard/Dash-Components/EditUser.js
+++ src/Dashboard/Dash-Components/EditUser.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 function EditUser(props) {
-  console.log(props.data);
+  const [fName, setFName] = useState("");
+
+  const UpdateUser = (e) => {};
   return (
     <div className="overlay position-fixed d-flex justify-content-center">
       <div className="user-modal align-self-center p-3 d-flex flex-column">
@@ -10,11 +12,21 @@ function EditUser(props) {
         </div>
         <h1 className="text-center req_h1">Edit Details</h1>
         <form>
-          <input type="text" placeholder="pick up:" className="input-home" />
-          <input type="text" placeholder="pick up:" className="input-home" />
-          <input type="text" placeholder="pick up:" className="input-home" />
-          <input type="text" placeholder="pick up:" className="input-home" />
-          <input type="text" placeholder="pick up:" className="input-home" />
+          <input
+            type="text"
+            className="input-home"
+            value={props.data.firstname}
+            onChange={(e) => setFName(e.target.value)}
+            name="fName"
+          />
+          <input
+            type="text"
+            className="input-home"
+            value={props.data.lastname}
+          />
+          <input type="text" className="input-home" value={props.data.email} />
+          <input type="text" className="input-home" value={props.data.phone} />
+          <input type="text" className="input-home" value={props.data.city} />
 
           <button className="my_btn w900">Save</button>
         </form>
