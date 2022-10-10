d01b873947e5263fe5e90e3f920b145e7f99214c
Young Batimehin <benny.bat51@gmail.com>
2022-10-10 11:46:16 +0100

    s

--- src/components/Underconstruction.js
+++ src/components/Underconstruction.js
@@ -6,7 +6,7 @@ function Underconstruction() {
   return (
     <div className="bg-white d-flex justify-content-center">
       <div className="text-center nf_div">
-      <img src={NF} alt='icon' className=" mb-5" />
+      <img src={NF} alt='icon' className=" mb-5 mt-5" />
                 <h1 className="nf_h1">Page currently under construction!</h1>
                 <p className="nf_p">Chat with support for more details</p>
  
