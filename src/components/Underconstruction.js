361cf6ee76aef256d74e3dee702154269a01fc8e
Young Batimehin <benny.bat51@gmail.com>
2022-10-10 11:45:05 +0100

    ur

--- src/components/Underconstruction.js
+++ src/components/Underconstruction.js
@@ -7,10 +7,9 @@ function Underconstruction() {
     <div className="bg-white d-flex justify-content-center">
       <div className="text-center nf_div">
       <img src={NF} alt='icon' className=" mb-5" />
-                <h1 className="nf_h1">Coming Soon!</h1>
-                <p className="nf_p">This feature will soon be realeased, it will enable customers talk directly with the drivers that are with the haulage!</p>
-                <p className="nf_sec_p">In the mean time, feel free to explore other features of the AKERU</p>
-
+                <h1 className="nf_h1">Page currently under construction!</h1>
+                <p className="nf_p">Chat with support for more details</p>
+ 
                 <Link to="/" className="btn btn-akeru">Go home</Link>
       </div>
     </div>
