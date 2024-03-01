1bb164ad136bf0786807da6e93506797a8746790
Young Batimehin <benny.bat51@gmail.com>
2024-03-01 15:07:38 +0100

    lk

--- src/Dashboard/Form.js
+++ src/Dashboard/Form.js
@@ -1,47 +1,47 @@
-import React from "react";
-import { useForm, useStep } from "react-hooks-helper";
-import Details from "./Details";
-import Settings from "./Settings";
-import Review from "./Review";
+// import React from "react";
+// import { useForm, useStep } from "react-hooks-helper";
+// import Details from "./Details";
+// import Settings from "./Settings";
+// import Review from "./Review";
 
-const defaultData = {
-  truck: "",
-  capacity: "",
-  contact: "",
-  phone: "",
-  email: "",
-  date: "",
-  pricing: "",
-  build: "",
-  fueature: "",
-  currency: ""
-};
+// const defaultData = {
+//   truck: "",
+//   capacity: "",
+//   contact: "",
+//   phone: "",
+//   email: "",
+//   date: "",
+//   pricing: "",
+//   build: "",
+//   fueature: "",
+//   currency: ""
+// };
 
-const steps = [
-    { id: "details" }, 
-    { id: "settings" }, 
-    { id: "review" }
-];
+// const steps = [
+//     { id: "details" }, 
+//     { id: "settings" }, 
+//     { id: "review" }
+// ];
 
-function Form() {
-  const [formData, setForm] = useForm(defaultData);
-  const { step, navigation } = useStep({
-    steps,
-    initialStep: 0,
-  });
+// function Form() {
+//   const [formData, setForm] = useForm(defaultData);
+//   const { step, navigation } = useStep({
+//     steps,
+//     initialStep: 0,
+//   });
 
-  const props = { formData, setForm, navigation };
+//   const props = { formData, setForm, navigation };
 
-  switch (step.id) {
-    case "details":
-      return <Details {...props}/>;
-    case "settings":
-      return <Settings {...props}/>;
-    case "review":
-      return <Review {...props}/>;
-  }
+//   switch (step.id) {
+//     case "details":
+//       return <Details {...props}/>;
+//     case "settings":
+//       return <Settings {...props}/>;
+//     case "review":
+//       return <Review {...props}/>;
+//   }
 
-  return <Details />;
-}
+//   return <Details />;
+// }
 
-export default Form;
+// export default Form;
